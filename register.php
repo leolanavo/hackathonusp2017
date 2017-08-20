dabede05a045f20575fbd58568fdeafe4461f690
Victor Seiji Hariki <victorseijih@gmail.com>
2017-08-19 21:50:00 -0300

    Updated index (Added login and registration forms)

---
+++ register.php
@@ -0,0 +1,25 @@
+<?php
+class MyDB extends SQLite3 {
+    function __construct() {
+        $this->open('database.db');
+    }
+}
+$db = new MyDB();
+if(!$db)
+    exit(-1);
+
+$name = $_POST["name"];
+$username = $_POST["username"];
+$password = $_POST["password"];
+$passhash = password_hash($password, PASSWORD_DEFAULT);
+$sql =
+    "SELECT id FROM users WHERE passhash='".$passhash."' AND (username='".$username."') OR (mail='".$username."')";
+
+$ret = $db->exec($sql);
+if(!$ret){
+    echo $db->lastErrorMsg();
+} else {
+    echo "Table created successfully\n";
+}
+$db->close();
+?>
